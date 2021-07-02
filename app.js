1bf9f13c69bfc3a08044f459e12d13b582019686
MikixIT <michael.46@live.it>
2021-07-02 19:32:07 +0200

    Layout web app

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-        })
-
-    } else {
-        h1.textContent = 'Hey we gotta a Problem, Contact me michaeltorrespa@gmail.com'
-        return
-    }
-});
+        
+console.log('JS Works!');

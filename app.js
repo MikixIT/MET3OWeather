2b94e57ab831d47ec9ed8c48cf31b8f563e5ebd9
MikixIT <michael.46@live.it>
2021-07-06 18:43:55 +0200

    Fix BG.jpg (creating img folder)

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-        
-console.log('JS Works!');
+const api = {
+   key: "b84be598b7ca04f0f84efa8c8c8ba92d",
+   baseurl: "https://home.openweathermap.org/api_keys"
+};
+
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keycode == 13) {
+        // getResult(searchBox.value);
+        console.log(searchBox)
+    }
+}
